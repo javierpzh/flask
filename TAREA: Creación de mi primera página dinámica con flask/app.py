cf0d0c7d38b71c247336fbfa04ff9b501da0e87d
Javier Pérez Hidalgo <reyole111@gmail.com>
2020-04-16 21:10:40 +0200

    Cuentaletras terminado

--- TAREA: Creación de mi primera página dinámica con flask/app.py
+++ TAREA: Creación de mi primera página dinámica con flask/app.py
@@ -1,8 +1,31 @@
 from flask import Flask, render_template,abort
 app = Flask(__name__)	
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def inicio():
 	return render_template("inicio.html")
+
+@app.route('/potencia/<int:base>/<exponente>',methods=["GET","POST"])
+def potencia(base,exponente):
+	exponente=int(exponente)
+	if exponente>0:
+		resultado = base**exponente
+	elif exponente==0:
+		resultado = 1
+	elif exponente<0:
+		resultado = base**exponente
+	else:
+		abort(404)
+	return render_template("potencia.html",base=base,exponente=exponente,res=resultado)
+
+
+@app.route('/cuenta/<palabra>/<letra>',methods=["GET","POST"])
+def cuentaletras(palabra,letra):
+	if len(letra)!=1:
+		abort(404)
+	else:
+		resultado=palabra.count(letra)
+	return render_template("cuentaletras.html",palabra=palabra,letra=letra,res=resultado)
+
 
 app.run(debug=True)
