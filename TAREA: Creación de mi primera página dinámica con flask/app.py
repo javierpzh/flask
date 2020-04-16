815183588a4c6d2887da484af5b11a7136f0dbb7
Javier Pérez Hidalgo <reyole111@gmail.com>
2020-04-16 21:25:00 +0200

    Libros terminado

--- TAREA: Creación de mi primera página dinámica con flask/app.py
+++ TAREA: Creación de mi primera página dinámica con flask/app.py
@@ -26,14 +26,13 @@ def cuentaletras(palabra,letra):
 		resultado=palabra.count(letra)
 	return render_template("cuentaletras.html",palabra=palabra,letra=letra,res=resultado)
 
-@app.route('/cuenta/<palabra>/<letra>',methods=["GET","POST"])
-def cuentaletras(palabra,letra):
-	if len(letra)!=1:
-		abort(404)
-	else:
-		resultado=palabra.count(letra)
-	return render_template("cuentaletras.html",palabra=palabra,letra=letra,res=resultado)
-
-
+@app.route('/libro/<int:cod>',methods=["GET","POST"])
+def codigolibro(cod):
+	libros=doc.xpath('//libro/codigo/text()')
+	for codigos in libros:
+		if cod in codigos:
+			autor=doc.xpath('/biblioteca/libro/autor/text()')
+			titulo=doc.xpath('/biblioteca/libro/titulo/text()')
+	return render_template("cuentaletras.html",libro=titulo,autor=autor)
 
 app.run(debug=True)
